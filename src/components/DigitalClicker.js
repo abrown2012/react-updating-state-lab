47d2efe5eae2e9180f30c4ab89e273d574c170f5
Agnieszka Brown <abrown2012@icloud.com>
2021-07-18 16:36:38 -0400

    commit tree b74926f4b7b847f58ac8702dbc3e973ac9a405ef

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    // Initial state is defined
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  // when handleClick is called, setState will update the state so that toggle is reversed
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    );
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+
+class YouTubeDebugger extends React.Component {
+    constructor() {
+      super();
+  
+      // Initial state is defined
+      this.state = {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      } }
+  
+    // when handleClick is called, setState will update the state so that toggle is reversed
+    handleClick = () => {
+      this.setState({
+        settings: {
+            ...this.state.settings, 
+            bitrate: 12
+        }
+      })
+    }
+
+    handleSecondClick = () =>  {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+  
+    render() {
+      return (
+        <div>
+          <button className='bitrate' onClick={this.handleClick}></button>
+          <button className='resolution' onClick={this.handleSecondClick}></button>
+        </div>
+      );
+    }
+  }
+  
+  export default YouTubeDebugger;
